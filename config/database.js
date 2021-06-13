3dc31aa46e3d7d7fee8f09f1782cf6d37f888f82
yolilufei <15201507672@163.com>
2021-06-13 22:32:30 +0800

    修复数据库地址

--- config/database.js
+++ config/database.js
@@ -1,3 +1,19 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '106.75.222.146'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
